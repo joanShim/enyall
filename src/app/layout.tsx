7540c1f4005e79b8e10273603d38d9eb0faa5ffb
Shim Jeong ah <35457850+joanShim@users.noreply.github.com>
2025-03-29 18:16:11 +0900

    Fix : PWA 정의 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import QueryProvider from "@/providers/query-provider";
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 import TabBar from "@/components/layout/TabBar";
+import Script from "next/script";
+
 const pretendard = localFont({
   src: "../fonts/PretendardVariable.woff2",
   display: "swap",
@@ -15,6 +17,7 @@ export const metadata: Metadata = {
   description:
     "즐거웠던 시간을 기록하고 공유하는 콘서트 리뷰 아카이브 | 공연장 시야 정보 | 콘서트 후기",
   icons: "/favicon.ico",
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({
@@ -26,11 +29,13 @@ export default function RootLayout({
     <QueryProvider>
       <NuqsAdapter>
         <html lang="ko">
+          <head></head>
           <body className={`${pretendard.variable} antialiased`}>
             <main className="relative mx-auto min-h-screen max-w-md pb-16 shadow-xl">
               {children}
             </main>
             <TabBar />
+            <Script src="/service-worker.js" />
           </body>
         </html>
       </NuqsAdapter>
--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,22 +1,21 @@
-import { MetadataRoute } from "next";
+import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "엔열",
-    short_name: "엔열",
+    name: "ENYALL",
+    short_name: "ENYALL",
     description:
       "즐거웠던 시간을 기록하고 공유하는 콘서트 리뷰 아카이브 | 공연장 시야 정보 | 콘서트 후기",
     start_url: "/",
-    lang: 'ko',
     display: "standalone",
+    lang: "ko",
     background_color: "#ffffff",
-    theme_color: "#5237C9",
+    theme_color: "#ffffff",
     icons: [
       {
         src: "/icons/icon-192x192.png",
         sizes: "192x192",
         type: "image/png",
-        purpose: "maskable",
       },
       {
         src: "/icons/icon-512x512.png",
