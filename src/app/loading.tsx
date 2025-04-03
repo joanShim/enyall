4ef67a45966049ce415e36b6295e69f00c55fbc8
Shim Jeong ah <35457850+joanShim@users.noreply.github.com>
2025-04-03 20:36:14 +0900

    Chore : 로딩애니메이션 비활성화

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,9 @@
-import RandomEyeLoader from "@/components/ui/RandomEyeLoader";
+// import RandomEyeLoader from "@/components/ui/RandomEyeLoader";
 
 export default function loading() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <RandomEyeLoader />
+      {/* <RandomEyeLoader /> */}
     </div>
   );
 }
